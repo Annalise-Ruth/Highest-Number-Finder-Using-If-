dc5abb883a6780457cf010a83642e2a13d38c681
Annie <annaliseruth.delacruz@gmail.com>
2024-10-16 23:38:21 +0800

    Definition

--- highest_number_finder.py
+++ highest_number_finder.py
@@ -5,10 +5,11 @@ var_2 = int(input("Input your second number please: "))
 var_3 = int(input("Input your third number please: "))
 var_4 = int(input("Input your fourth number please: "))
 var_5 = int(input("Input your fifth number please: "))
-#Use if statements to compare
+#Define the variables and use if statements to compare
+def find_highest (var_1, var_2, var_3, var_4, var_5):
+    pass
 #Compare var_1 to var_2, var_3, var_4, and var_5. If its greater print var_1
 #Compare var_2 to var_3, var_4, and var_5. If its greater print var_2
 #Compare var_3 to var_4 and var_5. If its greater print var_3
 #Compare var_4 to var_5. If its greater print var_4
 #Else, print var_5
-
